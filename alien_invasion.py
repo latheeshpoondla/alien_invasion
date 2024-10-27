d59f340172a02916f316b1835a33273f05ca3eab
Latheesh Poondla <140319526+latheeshpoondla@users.noreply.github.com>
2024-10-27 13:57:01 +0530

    background colour

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,21 +1,24 @@
 import pygame
 import sys
 class Alien_Invasion:
-    ''''Master class of the game'''
+    '''Master class of the game'''
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien_Invasion")
+        self.bg_color = (180, 255, 190)
         self.clock = pygame.time.Clock()
-    def run_game(self):
-        '''Main loop - display loop'''
+    def rungame(self):
+        '''Main Loop - display loop'''
         while True:
-            '''Event loop'''
             for event in pygame.event.get():
+                '''Event loop'''
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
             self.clock.tick(60)
+
 if __name__ == '__main__':
     ai = Alien_Invasion()
-    ai.run_game()
+    ai.rungame()
