59ef7d1f68402908011c51d5754172864de86858
Latheesh Poondla <140319526+latheeshpoondla@users.noreply.github.com>
2024-10-27 11:22:19 +0530

    game window

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,17 @@
-print("This is my first python project")
+import pygame
+import sys
+class Alien_Invasion:
+    ''''Master class of the game'''
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+if __name__ == '__main__':
+    ai = Alien_Invasion()
+    ai.run_game()
